16ea47eeada44a6b4d148cf82ae6a51403a1af07
David Henslovitz <dhense177@gmail.com>
2025-11-06 17:13:50 -0500

    SQL script for database creation of infrastructure tables

--- sql/infrastructure_features.sql
+++ sql/infrastructure_features.sql
@@ -13,20 +13,20 @@ CREATE TABLE infrastructure_features.infrastructure (
 );
 
 COMMENT ON COLUMN infrastructure_features.infrastructure.class IS 'Class of the infrastructure feature; values are "substation" or "power_line"';
-COMMENT ON COLUMN infrastructure_features.infrastructure.voltage IS 'Voltage of the infrastructure feature; values are in volts';
+COMMENT ON COLUMN infrastructure_features.infrastructure.voltage IS 'Unit: volts';
 
 -- transportation
-DROP TABLE IF EXISTS infrastructure_features.transportation;
-CREATE TABLE infrastructure_features.transportation (
-    transportation_feature_id UUID PRIMARY KEY,
-    class character varying(1000) NOT NULL,
-    geometry geometry(LineString, 4326) NOT NULL,
-    geometry_26986 geometry(LineString, 26986) NOT NULL,
-    source character varying(1000) NOT NULL
-);
+-- DROP TABLE IF EXISTS infrastructure_features.transportation;
+-- CREATE TABLE infrastructure_features.transportation (
+--     transportation_feature_id UUID PRIMARY KEY,
+--     class character varying(1000) NOT NULL,
+--     geometry geometry(LineString, 4326) NOT NULL,
+--     geometry_26986 geometry(LineString, 26986) NOT NULL,
+--     source character varying(1000) NOT NULL
+-- );
 
-COMMENT ON COLUMN infrastructure_features.transportation.class IS 'Class of the transportation feature; values are "motorway", "primary", "secondary", "tertiary", "unclassified", "residential", "living_street", "service", "unknown"';
+-- COMMENT ON COLUMN infrastructure_features.transportation.class IS 'Class of the transportation feature; values are "motorway", "primary", "secondary", "tertiary", "unclassified", "residential", "living_street", "service", "unknown"';
 
 -- Indexes
 CREATE INDEX ON infrastructure_features.infrastructure USING GIST (geometry_26986);
-CREATE INDEX ON infrastructure_features.transportation USING GIST (geometry_26986);
+-- CREATE INDEX ON infrastructure_features.transportation USING GIST (geometry_26986);
