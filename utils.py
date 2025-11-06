fda45aeb647093d4425a0c10882e7d770b16d9e1
David Henslovitz <dhense177@gmail.com>
2025-11-06 17:01:42 -0500

    Remove unneeded files

--- utils.py
+++
@@ -1,8 +0,0 @@
-
-
-def create_reprojected_geometry_col(gdf, geometry_col, new_geometry_col, srid):
-    gdf[new_geometry_col] = gdf[geometry_col]
-    gdf = gdf.set_geometry(new_geometry_col)
-    gdf = gdf.to_crs(srid)
-    gdf = gdf.set_geometry(geometry_col)
-    return gdf
