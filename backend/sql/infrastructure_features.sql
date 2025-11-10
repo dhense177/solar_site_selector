cc2a2d1a0db858ff5a30ebcc3424d11ef217b0bc
David Henslovitz <dhense177@gmail.com>
2025-11-10 16:51:59 -0500

    uncommented out table creation code

--- backend/sql/infrastructure_features.sql
+++ backend/sql/infrastructure_features.sql
@@ -16,17 +16,17 @@ COMMENT ON COLUMN infrastructure_features.infrastructure.class IS 'Class of the 
 COMMENT ON COLUMN infrastructure_features.infrastructure.voltage IS 'Unit: volts';
 
 -- transportation
--- DROP TABLE IF EXISTS infrastructure_features.transportation;
--- CREATE TABLE infrastructure_features.transportation (
---     transportation_feature_id UUID PRIMARY KEY,
---     class character varying(1000) NOT NULL,
---     geometry geometry(LineString, 4326) NOT NULL,
---     geometry_26986 geometry(LineString, 26986) NOT NULL,
---     source character varying(1000) NOT NULL
--- );
+DROP TABLE IF EXISTS infrastructure_features.transportation;
+CREATE TABLE infrastructure_features.transportation (
+    transportation_feature_id UUID PRIMARY KEY,
+    class character varying(1000) NOT NULL,
+    geometry geometry(LineString, 4326) NOT NULL,
+    geometry_26986 geometry(LineString, 26986) NOT NULL,
+    source character varying(1000) NOT NULL
+);
 
--- COMMENT ON COLUMN infrastructure_features.transportation.class IS 'Class of the transportation feature; values are "motorway", "primary", "secondary", "tertiary", "unclassified", "residential", "living_street", "service", "unknown"';
+COMMENT ON COLUMN infrastructure_features.transportation.class IS 'Class of the transportation feature; values are "motorway", "primary", "secondary", "tertiary", "unclassified", "residential", "living_street", "service", "unknown"';
 
 -- Indexes
 CREATE INDEX ON infrastructure_features.infrastructure USING GIST (geometry_26986);
--- CREATE INDEX ON infrastructure_features.transportation USING GIST (geometry_26986);
+CREATE INDEX ON infrastructure_features.transportation USING GIST (geometry_26986);
